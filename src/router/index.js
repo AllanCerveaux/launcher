564e36b2155d82653b5828746500852e6c4451a5
Callan <cerveauxallanjean@gmail.com>
2019-12-12 14:25:55 +0100

    reindent

--- src/router/index.js
+++ src/router/index.js
@@ -15,41 +15,46 @@ import Login from '../components/Auth/Login.vue'
 Vue.use(VueRouter)
 
 const routes = [
-{
-  path: '/',
-  redirect: '/game/game-1',
-  beforeEnter: ifAuthenticated
-},
-{
-  path: '/game',
-  redirect: '/game/game-1',
-  beforeEnter: ifAuthenticated
-},
-{
-  path:'/login',
-  name: 'login',
-  component: Login,
-  beforeEnter: ifNotAuthenticated
-},
-{
-  path: '/game/:name',
-  name: 'game-container',
-  component: GameContainer,
-  props: (route) => ({name: route.params.name || 'game-1'}),
-  beforeEnter: ifAuthenticated
-},
-{
-  path: '/chat',
-  name: 'chat-container',
-  component: ChatContainer,
-  beforeEnter: ifAuthenticated
-},
-{
-  path: '/news',
-  name: 'news-container',
-  component: NewsContainer,
-  beforeEnter: ifAuthenticated
-},
+  {
+    path: '/',
+    redirect: '/game/game-1',
+    beforeEnter: ifAuthenticated
+  },
+  {
+    path: '/game/:name',
+    name: 'game-container',
+    component: GameContainer,
+    props: (route) => ({name: route.params.name || 'game-1'}),
+    beforeEnter: ifAuthenticated
+  },
+  {
+    path:'/login',
+    name: 'login',
+    component: Login,
+    beforeEnter: ifNotAuthenticated
+  },
+  {
+    path: '/chat',
+    name: 'chat-container',
+    component: ChatContainer,
+    beforeEnter: ifAuthenticated
+  },
+  {
+    path: '/news',
+    name: 'news-container',
+    component: NewsContainer,
+    beforeEnter: ifAuthenticated
+  },
+  {
+    path: '/game/*',
+    redirect: '/game/game-1',
+    beforeEnter: ifAuthenticated
+  },
+  {
+    path:'*',
+    redirect : '/game/game-1',
+    beforeEnter: ifAuthenticated
+  }
 ]
 
 const router = new VueRouter({
