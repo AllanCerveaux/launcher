4ce196df13e3b00c84102c683a6dcc3177e369f5
Callan <cerveauxallanjean@gmail.com>
2019-12-15 16:37:30 +0100

    Update : Create ne file variables.scss WIP: Clear old scss and restart all

--- src/App.vue
+++ src/App.vue
@@ -20,28 +20,16 @@
 </script>
 
 <style lang="scss">
-body{
-  padding: 0;
-  margin: 0;
-}
-.app {
-  font-family: 'Roboto', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  background-image: url("https://picsum.photos/1280/720");
-  background-size: cover;
-  color: #212121;
-  height: 100vh;
-  display: grid;
-  grid-template-columns: .2fr repeat(4, 1.5fr);
-  grid-template-rows: 1fr repeat(4, 2fr);
-  grid-column-gap: 0px;
-  grid-row-gap: 0px; 
-  .wrapper{
-    grid-area: 2 / 2 / 6 / 6;
+  @import './assets/modules/variables.scss';
+  @import '~reset-css/sass/reset';
+
+  .app{
+    height: 100vh;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: .5fr repeat(4, 1fr);
+    grid-column-gap: 0px;
+    grid-row-gap: 0px;
+    font-family: $fonts;
   }
-}
-
-
-
 </style>
