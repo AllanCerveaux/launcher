b427d8403ec600b633eaf502bfca95a805082acf
Callan <cerveauxallanjean@gmail.com>
2019-12-16 00:19:36 +0100

    Add: animations in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { vuexfireMutation } from 'vuexfire' 
+// import { vuexfireMutation } from 'vuexfire' 
 
 // modules
 import auth from './auth'
 import validator from './validator'
+import animations from './animations'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-	state: {
-	},
+	state: {},
 	mutations: {
-		...vuexfireMutation
+		// ...vuexfireMutation
 	},
 	actions: {
 	},
 	modules: {
 		auth,
-		validator
+		validator,
+		animations
 	}
 })
