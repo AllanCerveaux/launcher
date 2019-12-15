19d07b9df1e33e4e575d87eb89873e1bca821b9d
Callan <cerveauxallanjean@gmail.com>
2019-12-15 16:38:45 +0100

    Remove : remove old scss for all components

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,20 +1,9 @@
 <template>
-	<section class="game">
-		<div class="game-articles">
-			<article class="article-body"
-			v-for="article in articles[name]" 
-			:key="article.title"
-			@click="goToArticle(article.url)" 
-			:style="`background: linear-gradient(rgba(33, 33, 33, .45), rgba(33, 33, 33, .45)), url(${article.image}); background-size: cover`">
-			<h2 class="article-title">{{article.title}}</h2>
-			<p>{{article.excerpt}}</p>
-		</article>
-	</div>
-	<div class="game-play">
-		<button>PLAY</button>
-		
-	</div>
-</section>	
+	<section class="game">	
+		<div class="game-play">
+			<button>PLAY</button>	
+		</div>
+	</section>	
 </template>
 
 <script>
@@ -61,45 +50,4 @@
 </script>
 
 <style lang="scss" scoped="">
-.game{
-	height: 100%;
-	display: flex;
-	justify-content: space-between;
-	align-items: flex-end;
-	.game-articles {
-		width: 60%;
-		display: flex;
-		justify-content: space-around;
-		.article-body {
-			&:hover{
-				transform: translateY(-.2em);
-			}
-			width: 25%;
-			color:#FFF;
-			cursor: pointer;
-			padding: 0 .5em;
-			border-radius: 2% 2%;
-			text-shadow: -1px 1px 0 black;
-			transition: transform .14s ease;
-			box-shadow: -1px 1px 1px rgba(33,33,33,.5);
-		}
-	}
-}
-.game-play {
-	width: 30%;
-	display: flex;
-	justify-content: flex-end;
-	button {
-		width: 10em;
-		height: 3em;
-		color: #FFF;
-		font-size: 2em;
-		font-weight: bold;
-		margin: 0 1em 2em 0;
-		background: #689F39;
-		background: linear-gradient(to top, #689F39, #8BC34B);
-		border-radius: 5px;
-		border: 2px #689F38 solid;
-	}
-}
 </style>
--- src/views/NewsContainer.vue
+++ src/views/NewsContainer.vue
@@ -11,8 +11,4 @@ export default{
 </script>
 
 <style lang="scss" scoped>
-	.news-wrapper {
-		padding: 1em;
-		color:#FFF;
-	}
 </style>
